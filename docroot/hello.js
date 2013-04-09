54f903c02abf26e89f8608282b9af47dd8b1393d
Francesco Gigli <jaramir@gmail.com>
2013-04-10 00:37:05 +0100

    Use AMDefine for RequireJs/Node compatibility

--- docroot/hello.js
+++ docroot/hello.js
@@ -1,5 +1,7 @@
 "use strict";
 
+if (typeof define !== 'function') { var define = require('amdefine')(module) }
+
 define(function() {
     return {
 	greet: function(name) {
@@ -7,4 +9,3 @@ define(function() {
 	}
     };
 });
-
